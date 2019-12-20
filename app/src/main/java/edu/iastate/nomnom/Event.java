9bf12314f051ee8b8c9f812ed0a4897f4efd0e14
Zachary Johnson <ztj1@iastate.edu>
2019-12-20 13:20:24 -0600

    Updated Event JavaDoc

--- app/src/main/java/edu/iastate/nomnom/Event.java
+++ app/src/main/java/edu/iastate/nomnom/Event.java
@@ -38,6 +38,18 @@ public class Event {
     @ColumnInfo(name = "imgRef")
     private String imgRef;
 
+    /**
+     * Constructor for an Event object
+     * @param eventID
+     * @param title
+     * @param food
+     * @param latitude
+     * @param longitude
+     * @param locationDetails
+     * @param startTime
+     * @param endTime
+     * @param imgRef
+     */
     @Ignore
     public Event(String eventID, String title, String food, double latitude, double longitude, String locationDetails, String startTime, String endTime, String imgRef) {
         this.eventId = eventID;
@@ -51,82 +63,161 @@ public class Event {
         this.imgRef = imgRef;
     }
 
+    /**
+     * Empty Event object constructor
+     */
     public Event() {
 
     }
 
+    /**
+     * Gets the eventId
+     * @return eventId
+     */
     public String getEventId() {
         return eventId;
     }
 
+    /**
+     * Sets the eventId
+     * @param eventId
+     */
     public void setEventId(String eventId) {
         this.eventId = eventId;
     }
 
+    /**
+     * Gets the title
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Gets the latitude
+     * @return latitude
+     */
     public double getLatitude(){
         return latitude;
     }
 
+    /**
+     * Sets the latitude
+     * @param latitude
+     */
     public void setLatitude(double latitude){
         this.latitude = latitude;
     }
 
+    /**
+     * Gets the longitude
+     * @return longitude
+     */
     public double getLongitude(){
         return longitude;
     }
 
+    /**
+     * Sets the longitude
+     * @param longitude
+     */
     public void setLongitude(double longitude){
         this.longitude = longitude;
     }
 
+    /**
+     * Gets the locationDetails
+     * @return locationDetails
+     */
     public String getLocationDetails() {
         return locationDetails;
     }
 
+    /**
+     * Sets the locationDetails
+     * @param locationDetails
+     */
     public void setLocationDetails(String locationDetails) {
         this.locationDetails = locationDetails;
     }
 
+    /**
+     * Gets the startTime
+     * @return startTime
+     */
     public String getStartTime() {
         return startTime;
     }
 
+    /**
+     * Sets the startTime
+     * @param startTime
+     */
     public void setStartTime(String startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Gets the endTime
+     * @return endTime
+     */
     public String getEndTime() {
         return endTime;
     }
 
+    /**
+     * Sets the endTime
+     * @param endTime
+     */
     public void setEndTime(String endTime) {
         this.endTime = endTime;
     }
 
+    /**
+     * Gets the food
+     * @return food
+     */
     public String getFood() {
         return food;
     }
 
+    /**
+     * Sets the food
+     * @param details
+     */
     public void setFood(String details) {
         this.food = details;
     }
 
+    /**
+     * Gets the imgRef
+     * @return imgRef
+     */
     public String getImgRef() {
         return imgRef;
     }
 
+    /**
+     * Sets the imgRef
+     * @param imgRef
+     */
     public void setImgRef(String imgRef) {
         this.imgRef = imgRef;
     }
 
+    /**
+     * Creates the string representation of this event
+     * @return Event String
+     */
     @Override
     public String toString() {
         return "Event{" +
